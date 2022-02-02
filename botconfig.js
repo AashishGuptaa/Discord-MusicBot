7cad0937ce8937f829d1f3374be40f99e3eb32c2
AashishGuptaa <98879976+AashishGuptaa@users.noreply.github.com>
2022-02-02 15:40:46 +0530

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,9 +4,9 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Token: process.env.Token || "OTM4MTY5NTg2ODc0OTkwNjky.YfmYgw.8sIHZ11DWgQUv8-bVmGdDtuNEOc", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "938169586874990692", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "xJQHCOXG20NHssb7Cl-csJXxu5FIYoGg", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
@@ -28,15 +28,15 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "host name or IP",
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "your password",
+    host: "lavalink.devin-dev.xyz",
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "lava123",
     secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "2ab143cb10a24d4e9c89bf069407868c", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "a0540c399658458cb71c99630de7dd09", //Spotify Client Secret
   },
 };
